360c27ed6f73a262fc10dfd45cddbd7e93ff0e38
chia hung <a0926223478@hotmail.com>
2021-03-19 16:06:16 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const Todo = require('./models/todo')
 
 const app = express()
 
+//藉由mongoose串接mongodb文件資料庫
 mongoose.connect("mongodb://localhost/todo-list", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -22,11 +23,14 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+//架設handlebars模板引擎
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+//使用bodyPareser捕捉POST的回傳資料
 app.use(bodyParser.urlencoded({ extended: true }))
 
+//從db讀取資料並渲染index頁面
 app.get('/', (req, res) => {
   Todo.find()
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
@@ -34,10 +38,12 @@ app.get('/', (req, res) => {
     .catch((error) => console.log(error));
 })
 
+//一個新增頁面
 app.get('/todos/new', (req, res) => {
   res.render('new')
 })
 
+//從ID比對用戶想取得的名字並進入detail頁面
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id
   return Todo.findById(id)
@@ -46,6 +52,7 @@ app.get('/todos/:id', (req, res) => {
   .catch(error => console.log(error))
 })
 
+//從id比對並進入編輯頁面
 app.get("/todos/:id/edit", (req, res) => {
   const id = req.params.id;
   return Todo.findById(id)
@@ -54,6 +61,7 @@ app.get("/todos/:id/edit", (req, res) => {
     .catch((error) => console.log(error));
 });
 
+//取得用戶想新增的資料，並把資料創建且回傳資料庫裡，然後渲染到idex頁面
 app.post('/todos', (req,res) =>{
   const name = req.body.name
   return Todo.create({ name })
@@ -61,6 +69,7 @@ app.post('/todos', (req,res) =>{
   .catch(error => console.log(error))
 })
 
+//在用戶指定的id中進入編輯頁面,然後將編輯後的資料取代原有的資料並儲存,然後渲染到index頁面
 app.post("/todos/:id/edit", (req, res) => {
   const id = req.params.id
   const name = req.body.name;
@@ -71,6 +80,15 @@ app.post("/todos/:id/edit", (req, res) => {
     })
     .then(() => res.redirect(`/todos/${id}`)) 
     .catch(error => console.log(error))
+});
+
+//在用戶指定的id,移除指定的id並渲染index畫面
+app.post("/todos/:id/delete", (req, res) => {
+  const id = req.params.id;
+  return Todo.findById(id)
+    .then(todo =>  todo.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error));
 });
 
 app.listen(3000, () => {
