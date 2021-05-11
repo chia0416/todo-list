46745011d163f38522135fbd9d8cb096d631fb64
chia hung <a0926223478@hotmail.com>
2021-05-11 17:16:30 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const User = require("../../models/user");
 
 router.get("/login", (req, res) => {
@@ -50,12 +51,14 @@ router.post("/register", (req, res) => {
         confirmPassword,
       });
     } else {
-      console.log('creating')
-      return User.create({
+     return bcrypt
+     .genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+     .then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+     .then(hash => User.create({
         name,
         email,
-        password,
-      })
+        password: hash, // 用雜湊值取代原本的使用者密碼
+      }))
       .then(() => res.redirect('/'))
       .catch(err => console.log(err))
     }
